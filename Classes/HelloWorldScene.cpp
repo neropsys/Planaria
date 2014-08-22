c222752ad5a4bea24e4f8ba6baa1f349b759f2c1
neropsys <neropsys@gmail.com>
2014-08-22 17:55:20 +0900

    removed some of the code from master branch on helloWorldScene.cpp, divded function to multiple lines

--- Classes/HelloWorldScene.cpp
+++ Classes/HelloWorldScene.cpp
@@ -80,11 +80,8 @@ bool HelloWorld::init()
     Size visibleSize = Director::getInstance()->getVisibleSize();
     Vec2 origin = Director::getInstance()->getVisibleOrigin();
 
-<<<<<<< HEAD
-    auto bgimage = Sprite::create("top.png");
-=======
     auto bgimage = Sprite::create("background/fishtank.png");
->>>>>>> origin/Planaria-Redesign
+
     bgimage->setPosition(visibleSize / 2);
     bgimage->setScale(1.35f);
     //bgimage->runAction(Liquid::create(2, Size(32, 32), 1, 20));
@@ -100,26 +97,11 @@ bool HelloWorld::init()
 
     // create a sequence an repeat it forever
     //bgimage->runAction(RepeatForever::create((Sequence*)Sequence::create(waves, lens, NULL)));
-<<<<<<< HEAD
-    //nodeGrid->runAction(RepeatForever::create((Sequence*)Sequence::create(waves, lens, ripple, NULL)));
-
-=======
-
-    auto shaky = Shaky3D::create(60, Size(32, 32), 3, true);
-
-    auto ripple = Ripple3D::create(60, Size(32, 32), visibleSize / 2, 800, 10, 50);
-    
->>>>>>> origin/Planaria-Redesign
     auto nodeGrid = NodeGrid::create();
 
     nodeGrid->runAction(RepeatForever::create((Sequence*)Sequence::create(ripple, lens, NULL)));
 
     nodeGrid->addChild(bgimage);
-<<<<<<< HEAD
-=======
-    //nodeGrid->runAction(Liquid::create(2, Size(32, 32), 1, 20));
-    nodeGrid->runAction(RepeatForever::create((Sequence*)Sequence::create(ripple, lens, NULL)));
->>>>>>> origin/Planaria-Redesign
 
     this->addChild(nodeGrid);
 
@@ -167,49 +149,6 @@ bool HelloWorld::init()
 
     this->addChild(Area::coinLabel);
 
-    /////////////////////////////
-    // 2. add a menu item with "X" image, which is clicked to quit the program
-    //    you may modify it.
-
-    // add a "close" icon to exit the progress. it's an autorelease object
-    /*auto knifeSkillOff = MenuItemImage::create(
-        "weapon/rose-knife-off.png",
-        "weapon/rose-knife-off.png",
-        NULL,
-        NULL);
-
-    auto knifeSkillOn = MenuItemImage::create(
-        "weapon/rose-knife-on.png",
-        "weapon/rose-knife-on.png",
-        NULL,
-        NULL);
-
-    auto roseKnife = MenuItemToggle::createWithCallback(CC_CALLBACK_1(HelloWorld::weaponMenuCallback, this), knifeSkillOff, knifeSkillOn, NULL);
-    roseKnife->setName("roseKnife");
-
-    auto scoopOff = MenuItemImage::create(
-        "weapon/scoop-pot-off.png",
-        "weapon/scoop-pot-off.png",
-        NULL,
-        NULL);
-
-    auto scoopOn = MenuItemImage::create(
-        "weapon/scoop-pot-on.png",
-        "weapon/scoop-pot-on.png",
-        NULL,
-        NULL);
-
-    auto scoopPot = MenuItemToggle::createWithCallback(CC_CALLBACK_1(HelloWorld::weaponMenuCallback, this), scoopOff, scoopOn, NULL);
-    scoopPot->setName("scoopPot");
-
-    // create menu, it's an autorelease object
-    auto menu = Menu::create(roseKnife, scoopPot, NULL);
-    menu->setPosition(128, 64);
-    this->addChild(menu, 1);
-    menu->setTag(_WEAPON_);
-    menu->alignItemsHorizontally();
-
-    roseKnife->activate();*/
 
     /*
     // 3. add your codes below...
